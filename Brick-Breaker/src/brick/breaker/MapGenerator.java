c96bda47ee9d6af89db9abaeb23040903fb42fcf
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-11-11 23:00:36 +0200

    added black stroke to bricks

--- Brick-Breaker/src/brick/breaker/MapGenerator.java
+++ Brick-Breaker/src/brick/breaker/MapGenerator.java
@@ -4,6 +4,7 @@
  */
 package brick.breaker;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
@@ -37,6 +38,9 @@ public class MapGenerator {
                 if(map[i][j] > 0){
                     g.setColor(Color.white);
                     g.fillRect(j*brickWidth+80, i*brickHeight+50, brickWidth, brickHeight);
+                    g.setStroke(new BasicStroke(3));
+                    g.setColor(Color.black);
+                    g.drawRect(j*brickWidth+80, i*brickHeight+50, brickWidth, brickHeight);
                 }
             }
         }
