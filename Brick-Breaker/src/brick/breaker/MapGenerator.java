551d512e32124f39b9469f23a49b0bbbcd54cd1e
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-11-11 22:46:17 +0200

    created map & bricks

--- Brick-Breaker/src/brick/breaker/MapGenerator.java
+++ Brick-Breaker/src/brick/breaker/MapGenerator.java
@@ -4,10 +4,42 @@
  */
 package brick.breaker;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+
 /**
  *
- * @author yasmin
+ * @author Firas
  */
 public class MapGenerator {
+    
+    
+    public int map[][];
+    public int brickWidth;
+    public int brickHeight;
+    
+    public MapGenerator(int row, int column){
+        
+        map = new int[row][column];
+        for(int i = 0; i < map.length; i++){
+            for(int j = 0; j < map[0].length; j++){
+                map[i][j] = 1;
+            }
+        }
+        
+        brickWidth = 540/column;
+        brickHeight = 150/row;
+    }
+    
+    public void draw(Graphics2D g){
+        for(int i = 0; i < map.length; i++){
+            for(int j = 0; j < map[0].length; j++){
+                if(map[i][j] > 0){
+                    g.setColor(Color.white);
+                    g.fillRect(j*brickWidth+80, i*brickHeight+50, brickWidth, brickHeight);
+                }
+            }
+        }
+    }
     
 }
